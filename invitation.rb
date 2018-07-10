173d526177aa631590a97a149823c0e743e925a1
Julia Guillemin <julia.guillemingirls@gmail.com>
2018-07-10 14:13:36 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,4 +1,19 @@
 
-puts "Hi, yiu have been invited to a party! What is your name?"
+puts "how are you inviting to the party"
 guest_name=gets.chomp
 
+puts "what is the name of your party?"
+party_name=gets.chomp
+
+puts "what is the date of your parts?"
+date=gets.chomp
+
+puts "what time will your party be at?"
+time-gets.chomp
+
+puts "what is your name?"
+host_name=gets.chomp
+
+
+puts "Dear #{guest_name}
+you have been invited to #{party_name} on #{date} at #{time} by #{host_name}
