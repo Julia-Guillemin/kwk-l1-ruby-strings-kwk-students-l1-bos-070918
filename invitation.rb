371bee41dcf9c10aec11e713567add9067a93345
Julia Guillemin <julia.guillemingirls@gmail.com>
2018-07-10 14:05:12 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,10 @@
 # Code your prompts here!
-
+guest_name=gets.chomped
+party_name=gets.chomped
+date=gets.chomped
+time-gets.chomped
+host_name=gets.chomped
+"Dear #{guest_name},
 # Try starting out with puts'ing a string.
+"Dear #{guest_name}
+you have been invited to #{party"
